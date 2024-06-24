c925c14162e76915d8bc521ff0f16e0178eadbb6
suhas <suhas8r@gmail.com>
2024-06-24 09:23:19 +0530

    Initial Commit

---
+++ login2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  login2
+//
+//  Created by SUHAS R on 24/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ login2/login2App.swift
@@ -0,0 +1,17 @@
+//
+//  login2App.swift
+//  login2
+//
+//  Created by SUHAS R on 24/06/24.
+//
+
+import SwiftUI
+
+@main
+struct login2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
